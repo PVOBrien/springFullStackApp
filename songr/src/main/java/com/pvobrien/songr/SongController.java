c652e49e6eb0a073ef44506a54b520df95b57db2
Paul OBrien <PVOVideo@outlook.com>
2020-09-30 19:41:01 -0700

    up to speed

---
+++ songr/src/main/java/com/pvobrien/songr/SongController.java
@@ -0,0 +1,22 @@
+package com.pvobrien.songr;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SongController {
+
+    @Autowired
+    AlbumRepository albumRepository;
+
+    @Autowired
+    SongRepository songRepository;
+
+    @PostMapping("/song")
+    public RedirectView addSong(String title, long AlbumId) {
+        Album thisOne = AlbumRepository.getOne(AlbumId); // getOne(AlbumId)
+        Song thisSong = new Song(title, 10, 3, thisOne); // length, trackNumber, thisOne
+    }
+}
