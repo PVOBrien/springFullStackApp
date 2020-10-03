e4430435f32d659f5d1e10145db7b5bfec8e8e97
Paul OBrien <PVOVideo@outlook.com>
2020-10-02 23:51:03 -0700

    stop for the night.

--- songr/src/main/java/com/pvobrien/songr/AlbumController.java
+++ songr/src/main/java/com/pvobrien/songr/AlbumController.java
@@ -39,29 +39,7 @@ public class AlbumController {
 
     @GetMapping("/albums")
     public String showAlbums(Model m){
-//        ArrayList<Album> albums = new ArrayList<>();
 
-//        albums.add( new Album(
-//                "Star Wars: The Phantom Menace",
-//                "John Williams",
-//                17,
-//                4_463,
-//                "https://upload.wikimedia.org/wikipedia/en/c/cc/John_Williams_%E2%80%93_Duel_Of_The_Fates_%28Promotional_Single_Artwork%29.jpg"
-//        ));
-//        albums.add(new Album(
-//                "Calling All Dawns",
-//                "Christopher Tin",
-//                12,
-//                2_756,
-//                "https://m.media-amazon.com/images/I/91yAHEkB-fL._SS500_.jpg"
-//        ));
-//        albums.add(new Album(
-//                "The Lord of the Rings: Return of the King",
-//                "Howard Shore",
-//                19,
-//                4325,
-//                "https://images-na.ssl-images-amazon.com/images/I/91%2Bni21hctL._SY355_.jpg"
-//        ));
         ArrayList<Album> albums = (ArrayList<Album>) albumRepository.findAll();
 
         m.addAttribute("albumsToShow", albums);
